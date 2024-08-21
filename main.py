9df956e9654e884a38e6c759ffb773f4139c4841
LanzSalviejo <lanzsalviejo@gmail.com>
2024-08-21 11:58:13 -0700

    added player movement

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         
-        # limit framerate to 60fps
-        dt = clock.tick(60) / 1000
+        # limit framerate
+        dt = clock.tick(144) / 1000
         
 if __name__ == "__main__":
     main()
