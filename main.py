87d4d1e060b08047c0deca5e4facb7f17867563b
LanzSalviejo <lanzsalviejo@gmail.com>
2024-08-21 12:05:49 -0700

    added containers

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        #player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+        #player.draw(screen)
         pygame.display.flip()
         
         # limit framerate
